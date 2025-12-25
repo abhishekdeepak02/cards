ea6242efa6bf87afa9558e1ae236cfc1627f2795
abhishekdeepak02 <yadu.akd@gmail.com>
2025-12-25 20:43:49 +0530

    enable actuator refresh endpoint to update config changes without re-start

--- src/main/java/com/lazybytes/cards/dto/CardsInfoRecordDto.java
+++ src/main/java/com/lazybytes/cards/dto/CardsInfoRecordDto.java
@@ -1,10 +1,18 @@
 package com.lazybytes.cards.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "cards")
-public record CardsInfoRecordDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+@Getter
+@Setter
+public class CardsInfoRecordDto{
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
+
 }
